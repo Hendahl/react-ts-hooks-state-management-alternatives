bebb1b8a5b9adf9ce7921f589c9f838817add2a2
Hendahl <lars.hendahl@gmail.com>
2020-06-18 08:38:29 +0200

    Removed duplicated redux store

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,7 @@ import * as serviceWorker from "./serviceWorker";
 import App from "./containers/app";
 import React from "react";
 import ReactDOM from "react-dom";
-import store from "./redux/store";
-import { Provider } from "react-redux";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 serviceWorker.unregister();
