45e5747e367792d5c4a4d1f8e9c8523672b0657d
Hendahl <lars.hendahl@gmail.com>
2020-11-03 08:24:56 +0100

    removed puppeteer

--- e2e/specs/index.js
+++
@@ -1,9 +0,0 @@
-import { load, getTitle, getScreenImage } from "../pageObjects/index";
-
-describe("React App", () => {
-  it("should be titled 'React Hooks Todos'", async () => {
-    await load();
-    expect(await getTitle()).toBe("React Hooks Todos");
-    await getScreenImage();
-  });
-});
